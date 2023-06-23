d262e9d2e0e2443ae66eb61867fb4773a8b67016
Prabin Byanjankar <prabinbyanjankar@Prabins-MacBook-Air.local>
2023-06-23 07:51:45 +0545

    Add setting items

--- app/src/main/java/com/datagrandeur/gonogo/SettingActivity.java
+++ app/src/main/java/com/datagrandeur/gonogo/SettingActivity.java
@@ -1,8 +1,16 @@
 package com.datagrandeur.gonogo;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +18,61 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        setupActionBar();
+
+        //load setting fragment
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MainPreferenceFragment()).commit();
     }
+
+    public static class MainPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_main);
+
+            Preference btnExport = findPreference("btnExport");
+            btnExport.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    // Start the other activity when the button preference is clicked
+                    Intent intent = new Intent(getActivity(), DataExportActivity.class);
+                    startActivity(intent);
+                    return true;
+                }
+            });
+        }
+
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            startActivity(new Intent(SettingActivity.this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void setupActionBar() {
+        ViewGroup rootView = (ViewGroup) findViewById(R.id.action_bar_root); //id from appcompat
+
+        if (rootView != null) {
+            View view = getLayoutInflater().inflate(R.layout.toolbar_setting, rootView, false);
+            rootView.addView(view, 0);
+
+            setSupportActionBar(findViewById(R.id.toolbar));
+
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
 }
