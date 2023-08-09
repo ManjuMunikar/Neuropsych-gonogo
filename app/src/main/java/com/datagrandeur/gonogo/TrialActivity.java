9cc3caf3324986ef813fba03e9a8ab8cccb78589
Prabin Byanjankar <prabinbyanjankar@Prabins-MacBook-Air.local>
2023-08-09 18:19:12 +0545

    Practice Trail messaging works

--- app/src/main/java/com/datagrandeur/gonogo/TrialActivity.java
+++ app/src/main/java/com/datagrandeur/gonogo/TrialActivity.java
@@ -3,17 +3,17 @@ package com.datagrandeur.gonogo;
 import static androidx.fragment.app.FragmentManager.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.datagrandeur.gonogo.data.DatabaseHelper;
 import com.datagrandeur.gonogo.data.Trial;
@@ -22,96 +22,168 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Timer;
-import java.util.TimerTask;
 
 public class TrialActivity extends AppCompatActivity {
     List<String> stimuli = new ArrayList<>();
-    private int currentImageIndex = 0;
-    private Timer timer;
+    private int currentImageIndex = -1;
+    private CountDownTimer timer;
+    private Timer timer2;
     private ImageButton btnTrial;
-
+    private TextView tvMessage;
     private String packageName;
+    private Trial trial;
+
+    private boolean tapped = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trial);
-        currentImageIndex=0;
-
+        currentImageIndex = -1;
+        tvMessage = findViewById(R.id.tvMessage);
         btnTrial = findViewById(R.id.btnTrial);
         packageName = this.getPackageName();
 
         DatabaseHelper db = new DatabaseHelper(this);
-        Trial trial = db.getConfig(Singleton.getInstance().getTrialId());
+        trial = db.getConfig(Singleton.getInstance().getTrialId());
 
-        List<String> goFaces = db.getStimuluses(trial.getGoFace(), Singleton.getInstance().getLocation() ,trial.getGoFaceCount());
-        List<String> noGOFaces = db.getStimuluses(trial.getNoGoFace(), Singleton.getInstance().getLocation() ,trial.getNoGoFaceCount());
-
-        stimuli.addAll(resizeList(db.getStimuluses(trial.getGoFace(), Singleton.getInstance().getLocation(),trial.getGoFaceCount()),trial.getGoFaceCount() ));
-        stimuli.addAll(resizeList(db.getStimuluses(trial.getNoGoFace(), Singleton.getInstance().getLocation() ,trial.getNoGoFaceCount()),trial.getNoGoFaceCount()));
+        stimuli.addAll(resizeList(db.getStimuluses(trial.getGoFace(), Singleton.getInstance().getLocation(), trial.getGoFaceCount()), trial.getGoFaceCount()));
+        stimuli.addAll(resizeList(db.getStimuluses(trial.getNoGoFace(), Singleton.getInstance().getLocation(), trial.getNoGoFaceCount()), trial.getNoGoFaceCount()));
 
         Collections.shuffle(stimuli);
 
         btnTrial.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View v) {
 
-                System.out.println("Button pressed");
+                tapped = true;
 
-
+//                if (currentImageIndex >= 0) {
+//                    System.out.println(trial.getGoFace());
+//                    System.out.println(trial.getTrialName());
+//
+//
+//                    if ("Practice".equalsIgnoreCase(trial.getTrialName())) {
+//                        if (stimuli.get(currentImageIndex).contains("surprised")) {
+//                            tvMessage.setText(R.string.Correct);
+//                            tvMessage.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.Green));
+//                        } else {
+//                            tvMessage.setText(R.string.Incorrect);
+//                            tvMessage.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.Red));
+//                        }
+//                    }
+//
+//                    System.out.println("Button Pressed:" + stimuli.get(currentImageIndex));
+//                }
             }
         });
 
+
         startTimer();
 
-
     }
-
 
     private void startTimer() {
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(currentImageIndex >= stimuli.size()){
-                            currentImageIndex = 0;
-                            Intent intent = new Intent(getApplicationContext(), EndTrialActivity.class);
-                            finishAffinity();
-                            startActivity(intent);
-                        }else {
+        if (currentImageIndex >= stimuli.size() - 1) {
+            btnTrial.setImageResource(R.drawable.endplus);
+            new Handler().postDelayed(this::endActivity, 1000);
+        } else {
+            tvMessage.setText("");
+            tapped = false;
+            currentImageIndex++;
+            showNextImage();
+            new Handler().postDelayed(this::waitForInput, 500);
 
-                            new CountDownTimer(1000, 1000) {
-
-                                public void onTick(long millisUntilFinished) {
-                                    showNextImage();
-                                }
-
-                                public void onFinish() {
-                                    currentImageIndex++;
-
-                                    if(currentImageIndex >= stimuli.size()) {
-                                        btnTrial.setImageResource(R.drawable.endplus);
-                                    }else{
-                                        btnTrial.setImageResource(R.drawable.trial_plus);
-                                    }
-
-                                }
-                            }.start();
-
-                        }
-
-                    }
-                });
-            }
-        }, 4000, 4000);
+        }
     }
+
+    private void waitForInput() {
+        btnTrial.setImageResource(R.drawable.trial_plus);
+        new Handler().postDelayed(this::showAnswer, 1000);
+    }
+
+    private void showAnswer() {
+        if ("Practice".equalsIgnoreCase(trial.getTrialName())) {
+            if (isInputCorrect()) {
+                tvMessage.setText(R.string.Correct);
+                tvMessage.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.Green));
+            } else {
+                tvMessage.setText(R.string.Incorrect);
+                tvMessage.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.Red));
+            }
+        }
+        new Handler().postDelayed(this::startTimer, 500);
+    }
+
+    private boolean isInputCorrect() {
+        return (stimuli.get(currentImageIndex).contains("surprised") && tapped)
+                ||
+                (!stimuli.get(currentImageIndex).contains("surprised") && !tapped)
+                ;
+
+    }
+
+    private void endActivity() {
+        Intent intent = new Intent(getApplicationContext(), EndTrialActivity.class);
+        finishAffinity();
+        startActivity(intent);
+    }
+
+//    private void startTimer() {
+//        timer = new Timer();
+//
+//
+//        timer.schedule(new TimerTask() {
+//            @Override
+//            public void run() {
+//
+//                runOnUiThread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        if(currentImageIndex >= stimuli.size()-1){
+//                            Intent intent = new Intent(getApplicationContext(), EndTrialActivity.class);
+//                            finishAffinity();
+//                            startActivity(intent);
+//                        }else {
+//
+//                            new CountDownTimer(500, 500 ) {
+//
+//
+//                                public void onTick(long millisUntilFinished) {
+//                                    currentImageIndex++;
+//                                    tvMessage.setText("");
+//                                    showNextImage();
+//                                }
+//
+//                                public void onFinish() {
+//
+//                                    if(currentImageIndex >= stimuli.size()-1) {
+//                                        btnTrial.setImageResource(R.drawable.endplus);
+//                                    } else {
+//                                        btnTrial.setImageResource(R.drawable.trial_plus);
+//                                    }
+//
+//
+//
+//                                }
+//                            }.start();
+//
+//                        }
+//
+//                    }
+//                });
+//            }
+//        }, 3000, 3000);
+//    }
 
     @SuppressLint("RestrictedApi")
     private void showNextImage() {
+
         Log.d(TAG, "Index:" + currentImageIndex);
         Log.d(TAG, "Index File:" + stimuli.get(currentImageIndex) );
+        System.out.println("Current Image"+stimuli.get(currentImageIndex));
         btnTrial.setImageResource(getResources().getIdentifier(stimuli.get(currentImageIndex), "drawable", packageName));
     }
 
@@ -127,11 +199,11 @@ public class TrialActivity extends AppCompatActivity {
     private List<String> resizeList(List<String> strings, int size) {
 
         List<String > strList = new ArrayList<>();
-        while(strList.size()<size){
+        while(strList.size()<=size){
             strList.addAll(strings);
         }
 
-        return strList.subList(0,size-1);
+        return strList.subList(0,size);
 
     }
 
